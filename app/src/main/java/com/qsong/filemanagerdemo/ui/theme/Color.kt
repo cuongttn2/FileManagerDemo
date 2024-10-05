1f91990abfe202063a1e6f17b10699a985b1edd0
_v <caocuongttn2gmail.com>
2024-10-05 19:32:38 +0700

    setup font and color

--- app/src/main/java/com/qsong/filemanagerdemo/ui/theme/Color.kt
+++ app/src/main/java/com/qsong/filemanagerdemo/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package com.qsong.filemanagerdemo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryColor = Color(0xFF6200EE)
+val PrimaryLightColor = Color(0xFFBB86FC)
+val PrimaryDarkColor = Color(0xFF3700B3)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SecondaryColor = Color(0xFF03DAC6)
+val SecondaryLightColor = Color(0xFF66FFF9)
+val SecondaryDarkColor = Color(0xFF00A896)
+
+val BackgroundColor = Color(0xFFFFFFFF)
+val SurfaceColor = Color(0xFF121212)
+
+val ErrorColor = Color(0xFFB00020)
+val OnPrimaryColor = Color(0xFFFFFFFF)
